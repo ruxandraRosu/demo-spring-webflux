61acb642cc96967d5278c1a600eea6b6b3425283
ruxandraRosu <40659997+ruxandraRosu@users.noreply.github.com>
2024-04-06 05:50:56 +0300

    Update KafkaService.java

--- src/main/java/com/example/service/KafkaService.java
+++ src/main/java/com/example/service/KafkaService.java
@@ -1,9 +1,13 @@
 package com.example.service;
 
+import com.example.model.response.Trade;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.reactive.ReactiveKafkaProducerTemplate;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
 
 @Slf4j
 @AllArgsConstructor
@@ -11,11 +15,24 @@ import org.springframework.stereotype.Service;
 public class KafkaService {
 
     public static final String TOPIC_NAME = "topic-name";
-    private ReactiveKafkaProducerTemplate reactiveKafkaProducer;
+    public static final String TOPIC_NAME_TRADES = "topic-trades";
+    private ReactiveKafkaProducerTemplate<String, String> reactiveKafkaProducer;
+    private ObjectMapper mapper;
 
-    public void sendMessage(String value){
-        reactiveKafkaProducer.send(TOPIC_NAME, value)
-                .doOnSuccess(senderResult -> log.info("sent {}", value))
-                .subscribe();
+    public Mono sendMessage(Mono<Trade> trade) {
+
+        return trade.map(this::getValueAsString)
+                .flatMap(message -> reactiveKafkaProducer.send(TOPIC_NAME_TRADES, message))
+                .doOnNext(message -> log.info("After kafka" + message.toString()));
     }
+
+    private String getValueAsString(Trade trade) {
+        try {
+            return mapper.writeValueAsString(trade);
+        } catch (JsonProcessingException e) {
+            log.error("Unable to serialize object", e);
+            return "";
+        }
+    }
+
 }
