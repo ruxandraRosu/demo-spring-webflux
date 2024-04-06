70a6bccbb73881445cdd9d69c5332a73893c9d8b
ruxandraRosu <40659997+ruxandraRosu@users.noreply.github.com>
2024-04-06 05:42:17 +0300

    Create TradesController.java

---
+++ src/main/java/com/example/controller/TradesController.java
@@ -0,0 +1,26 @@
+package com.example.controller;
+
+import com.example.model.ProductInfo;
+import com.example.service.TradesService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+@Slf4j
+@AllArgsConstructor
+@RequestMapping("api")
+public class TradesController {
+    private TradesService tradesService;
+
+
+    @GetMapping("/products/{productId}/info")
+    public Mono<ProductInfo> getProductStats(@PathVariable String productId) {
+        log.info("{} thread", Thread.currentThread());
+        return tradesService.getProductInfo(productId);
+    }
+}
