bf13191ef4ef05d05d94f47ba6ce74e745263d2a
ruxandraRosu <40659997+ruxandraRosu@users.noreply.github.com>
2024-04-06 05:51:33 +0300

    Create MatchService.java

---
+++ src/main/java/com/example/service/MatchService.java
@@ -0,0 +1,56 @@
+package com.example.service;
+
+
+import com.example.model.MappingResolver;
+import com.example.websockets.TradeFlux;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.socket.WebSocketMessage;
+import org.springframework.web.reactive.socket.client.ReactorNettyWebSocketClient;
+import org.springframework.web.reactive.socket.client.WebSocketClient;
+import reactor.core.publisher.Mono;
+
+import javax.annotation.PostConstruct;
+import java.net.URI;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class MatchService {
+
+    @Value("${coinbase.endpoint}")
+    private String url;
+    @Value("${coinbase.matcher-message}")
+    private String matcherMessage;
+    private final MappingResolver mappingResolver;
+    private final TradeFlux tradeFlux;
+    private final TradesService tradesService;
+
+
+    @PostConstruct
+    public void afterInit() {
+        WebSocketClient client = new ReactorNettyWebSocketClient();
+        Mono<Void> result = client.execute(URI.create(url),
+                (session) -> session.send(Mono.just(session.textMessage(matcherMessage)))
+                        .ignoreElement()
+                        .thenMany(session.receive()
+                                .map(WebSocketMessage::getPayloadAsText)
+                                .log()
+                                .filter(v -> !v.contains("subscriptions"))
+                                .mapNotNull(mappingResolver::mapStringToMatch)
+                                .map(mappingResolver::mapMatchToTrade)
+//                                .zipWith(trade -> myService.getProduct(trade), (trade, product) -> Trade.builder()::product(product))
+                                .map(tradesService::decorateTrade)
+                                .map(tradesService::publishMessage)
+//                                .doOnNext(tradeFlux::push)
+                        )
+                        .then()
+        );
+        result.subscribe();
+    }
+
+
+
+}
