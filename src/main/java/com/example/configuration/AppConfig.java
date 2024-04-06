4be75d4c366cc9f633409f538fd363b4899ad830
ruxandraRosu <40659997+ruxandraRosu@users.noreply.github.com>
2024-04-06 05:42:59 +0300

    Update AppConfig.java

--- src/main/java/com/example/configuration/AppConfig.java
+++ src/main/java/com/example/configuration/AppConfig.java
@@ -1,10 +1,12 @@
 package com.example.configuration;
 
 import com.example.handlers.UserHandler;
-import com.example.service.MyService;
+import com.example.model.SubscriberInfo;
+import com.example.service.TradesService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,9 +22,9 @@ import reactor.kafka.sender.SenderOptions;
 import reactor.netty.http.client.HttpClient;
 import reactor.netty.resources.ConnectionProvider;
 
-
 import java.time.Duration;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
@@ -48,12 +50,12 @@ public class AppConfig {
     }
 
     @Bean
-    public UserHandler userHandler(MyService myService) {
-        return new UserHandler(myService);
+    public UserHandler userHandler(TradesService tradesService) {
+        return new UserHandler(tradesService);
     }
 
     @Bean
-    public WebClient webClient() {
+    public WebClient webClient(@Value("${coinbase-rest.endpoint}") String endpoint) {
         HttpClient httpClient = HttpClient.create(ConnectionProvider.builder("myConnectionProvider")
                 .maxConnections(60000)
                 .maxIdleTime(Duration.ofSeconds(20))
@@ -62,24 +64,30 @@ public class AppConfig {
                 .pendingAcquireMaxCount(100000)
                 .build());
         return WebClient.builder()
-                .baseUrl("http://localhost:8084")
+                .baseUrl(endpoint)
                 .clientConnector(new ReactorClientHttpConnector(httpClient))
                 .build();
     }
-
+    
     @Bean
-    public ReactiveKafkaProducerTemplate reactiveKafkaProducerTemplate(KafkaProperties properties) {
+    public ReactiveKafkaProducerTemplate<String, String> reactiveKafkaProducer(KafkaProperties properties) {
+
         Map<String, Object> props = properties.buildProducerProperties();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
 
-        return new ReactiveKafkaProducerTemplate(SenderOptions.create(props));
+        return new ReactiveKafkaProducerTemplate<String, String>(SenderOptions.create(props));
     }
-     @Bean
+
+    @Bean
     public ObjectMapper objectMapper() {
         return new ObjectMapper();
     }
 
+    @Bean
+    public Map<String, SubscriberInfo> subscribersMap() {
+        return new ConcurrentHashMap<>();
+    }
 
 }
