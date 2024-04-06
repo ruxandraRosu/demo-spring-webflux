7ef4b77fd5de02a9436726fbc597909450543148
ruxandraRosu <40659997+ruxandraRosu@users.noreply.github.com>
2024-04-06 05:53:16 +0300

    Create TradeFlux.java

---
+++ src/main/java/com/example/websockets/TradeFlux.java
@@ -0,0 +1,30 @@
+package com.example.websockets;
+
+import com.example.model.MappingResolver;
+import com.example.model.SubscriberInfo;
+import com.example.model.response.Trade;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Component
+@Slf4j
+@AllArgsConstructor
+public class TradeFlux {
+
+    private final Map<String, SubscriberInfo> subscribersMap;
+    private final MappingResolver mapper;
+    private final MessageMatcher matcher;
+
+    public void push(Trade trade) {
+        subscribersMap.entrySet().stream()
+                .filter(e -> matcher.matches(trade, subscribersMap.get(e.getKey()).getMessage().getFilters()))
+                .forEach(e -> e.getValue().getSink().next(mapper.writeTradeToString(trade)));
+
+    }
+
+
+
+}
